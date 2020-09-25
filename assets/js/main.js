e3bd9aa9729561f5da1b821e7382f216c6984a82
Ngugisenior <ngugijoseph992@gmail.com>
2020-09-25 16:14:03 +0200

    Ui components fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,10 @@ register.addEventListener('click', registerStud);
 //Company View
 function companyV(){
     main.innerHTML = `
-    <iframe class="airtable-embed" src="https://airtable.com/embed/shr6UuVuzN2ipB6N7?backgroundColor=green&layout=card&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="600" style="background: transparent; min-height:100vh; border: 1px solid #ccc;"></iframe>
+    <iframe class="airtable-embed" 
+    src="https://airtable.com/embed/shr6UuVuzN2ipB6N7?backgroundColor=green&layout=card&viewControls=on" 
+    frameborder="0" onmousewheel="" 
+    width="80%" height="2500px" style="background: transparent; border: 1px solid #ccc;"></iframe>
     `;
 };
 
@@ -18,11 +21,11 @@ function registerStud(){
     main.innerHTML=`
     <script 
     src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
-    <iframe class="airtable-embed" 
+    <iframe  class="airtable-embed" 
     src="https://airtable.com/embed/shrkx03RGPiF9yW1g?backgroundColor=teal" 
     frameborder="0" onmousewheel="" 
-    width="100%" 
-    height="600px" style="background: transparent;min-height:100vh;"></iframe>
+    width="80%" 
+    height="3200" style="background: transparent;"></iframe>
     `;
 };
 
